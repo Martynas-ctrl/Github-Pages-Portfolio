03120146376d6421509e05ace4930b5ae2448526
Martynas Majaras <martynasmajaras98@gmail.com>
2021-09-07 12:58:18 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/Header.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Victor är Bög :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="l-header">
+        <nav className="nav">
+          <div>
+            <a className="nav_logo" href="#">Martynas</a>
+          </div>
+          
+          <div className="nav__menu" id="nav-menu">
+            <ul className="nav__list">
+                <li className="nav__item"><a id="nav__link" href="#" target="_blank" rel="noopener noreferrer">Hem</a></li>
+                <li className="nav__item"><a id="nav__link" href="#" target="_blank" rel="noopener noreferrer">Om</a></li>
+                <li className="nav__item"><a id="nav__link" href="#" target="_blank" rel="noopener noreferrer">Kunskap</a></li>
+                <li className="nav__item"><a id="nav__link" href="#" target="_blank" rel="noopener noreferrer">Service</a></li>
+                <li className="nav__item"><a id="nav__link" href="#" target="_blank" rel="noopener noreferrer">Portfolio</a></li>
+                <li className="nav__item"><a id="nav__link" href="#" target="_blank" rel="noopener noreferrer">Kontakta Mig</a></li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
     </div>
   );
 }
